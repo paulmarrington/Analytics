04a8fc339eebc17b956e3ce12c687295008ff092
paulmarrington <paul@marrington.net>
2018-04-06 10:49:02 +1000

    make decoupled references volatile

--- Assets/Askowl/Analytics/Examples/AnalyticsExamples.cs
+++ Assets/Askowl/Analytics/Examples/AnalyticsExamples.cs
@@ -3,18 +3,16 @@ using JetBrains.Annotations;
 using UnityEngine;
 
 public sealed class AnalyticsExamples : MonoBehaviour {
-  private Analytics log;
-
-  private void Start() { log = Analytics.Instance; }
+  private static Analytics Log { get { return Analytics.Instance; } }
 
   [UsedImplicitly]
   public void EventExampleButton() {
-    log.Event("AnalyticsExamples", "Test Event", "Shown in Log",
+    Log.Event("AnalyticsExamples", "Test Event", "Shown in Log",
               "button=", "EventExampleButton", "count=", 33, true);
   }
 
   [UsedImplicitly]
   public void ErrorExampleButton() {
-    log.Error("Test Error", "Shown in Log also", "button=", "ErrorExampleButton", 44, false);
+    Log.Error("Test Error", "Shown in Log also", "button=", "ErrorExampleButton", 44, false);
   }
 }
